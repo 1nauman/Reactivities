16bc7552b1eda611d39b6bcb0c422aeecf5abdb1
Nauman Mohammed <nauman.rehman@gmail.com>
2020-08-13 02:03:41 +0530

    added aspnetcore identity (jwt based auth)

--- Persistence/Migrations/20200711204927_SeedValues.Designer.cs
+++ Persistence/Migrations/20200711204927_SeedValues.Designer.cs
@@ -15,7 +15,7 @@ namespace Persistence.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "5.0.0-preview.6.20312.4");
+                .HasAnnotation("ProductVersion", "3.1.7");
 
             modelBuilder.Entity("Domain.Value", b =>
                 {
--- Persistence/Migrations/20200712124606_ActivityEntityAdded.Designer.cs
+++ Persistence/Migrations/20200712124606_ActivityEntityAdded.Designer.cs
@@ -16,7 +16,7 @@ namespace Persistence.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "5.0.0-preview.6.20312.4");
+                .HasAnnotation("ProductVersion", "3.1.7");
 
             modelBuilder.Entity("Domain.Activity", b =>
                 {
